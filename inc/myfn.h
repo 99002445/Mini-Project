622e2de2c4de3c28a4eac030e7a8a01944035a3c
99002445 <71484839+99002445@users.noreply.github.com>
2020-09-18 23:32:31 +0530

    Update myfn.h

--- inc/myfn.h
+++ inc/myfn.h
@@ -1,11 +1,20 @@
-#ifndef __MYFN_H__
-#define __MYFN_H__
+#ifndef __NIRA_H__
+#define __NIRA_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
 
+void menu();
+void got();
+void start();
+void back();
+void addrecord();
+void listrecord();
+void modifyrecord();
+void deleterecord();
+void searchrecord();
 
-int printop(int a);
-
-#endif  
+#endif
