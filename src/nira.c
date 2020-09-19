0d695ce3c658a8df95cd19cbdda5ab596949984d
99002445 <71484839+99002445@users.noreply.github.com>
2020-09-19 07:48:13 +0530

    Update nira.c

--- src/nira.c
+++ src/nira.c
@@ -1,301 +1,59 @@
-#include<nira.h>
-
-void back()
+//function for displaying the result
+void display(float n1, float n2, char ch, float result)
 {
-    start();
+  printf("%.2f %c %.2f = %.2f\n", n1, ch, n2, result);
 }
-void start()
+
+//function for addition of two numbers
+void add(float n1, float n2)
 {
-    menu();
+  float result = n1 + n2;
+  display(n1, n2, '+', result);
 }
-void menu()
+
+//function for subtraction of two numbers
+void subtract(float n1, float n2)
 {
-    system("cls");
-printf("\t\t**********WELCOME TO PHONEBOOK*************");
-
-printf("\n\n\t\t\t  MENU\t\t\n\n");
-printf("\t1.Add New   \t2.List   \t3.Exit  \n\t4.Modify \t5.Search\t6.Delete");
-
-switch(getch())
-{
-    case '1':
-
-                addrecord();
-    break;
-   case '2': listrecord();
-    break;
-    case '3': exit(0);
-    break;
-    case '4': modifyrecord();
-    break;
-    case '5': searchrecord();
-    break;
-    case '6': deleterecord();
-    break;
-    default:
-                system("cls");
-                printf("\nEnter 1 to 6 only");
-                printf("\n Enter any key");
-                getch();
-
-menu();
+  float result = n1 - n2;
+  display(n1, n2, '-', result);
 }
+
+//function for multiplication of two numbers
+void multiply(float n1, float n2)
+{
+  float result = n1 * n2;
+  display(n1, n2, '*', result);
 }
-        void addrecord()
+
+//function for division of two numbers
+void divide(float n1, float n2)
 {
-        system("cls");
-        FILE *f;
-        struct person p;
-        f=fopen("project","ab+");
-        printf("\n Enter name: ");
-        got(p.name);
-        printf("\nEnter the address: ");
-        got(p.address);
-        printf("\nEnter father name: ");
-        got(p.father_name);
-        printf("\nEnter mother name: ");
-        got(p.mother_name);
-        printf("\nEnter phone no.:");
-        scanf("%ld",&p.mble_no);
-        printf("Enter sex:");
-        got(p.sex);
-        printf("\nEnter e-mail:");
-         got(p.mail);
-        printf("\nEnter citizen no:");
-        got(p.citision_no);
-        fwrite(&p,sizeof(p),1,f);
-
-      fflush(stdin);
-        printf("\nrecord saved");
-
-fclose(f);
-
-    printf("\n\nEnter any key");
-
-	 getch();
-    system("cls");
-    menu();
+  float result = n1 / n2;
+  display(n1, n2, '/', result);
 }
-void listrecord()
-{
-    struct person p;
-    FILE *f;
-f=fopen("project","rb");
-if(f==NULL)
-{
-printf("\nfile opening error in listing :");
 
-exit(1);
-}
-while(fread(&p,sizeof(p),1,f)==1)
+//function for calculating remainder
+void rem(float n1, float n2)
 {
-     printf("\n\n\n YOUR RECORD IS\n\n ");
-        printf("\nName=%s\nAdress=%s\nFather name=%s\nMother name=%s\nMobile no=%ld\nSex=%s\nE-mail=%s\nCitizen no=%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
+  //Modulus operator only works on int data type
+  //Floating numbers are converted to int number
+  int num1 = n1;
+  int num2 = n2;
+  int result = num1%num2;
+  printf("%d %% %d = %d\n", num1, num2, result);
+}
 
-	 getch();
-	 system("cls");
-}
-fclose(f);
- printf("\n Enter any key");
- getch();
-    system("cls");
-menu();
-}
-void searchrecord()
+//function for calculating power
+void power(float n1, float n2)
 {
-    struct person p;
-FILE *f;
-char name[100];
-
-f=fopen("project","rb");
-if(f==NULL)
-{
-    printf("\n error in opening\a\a\a\a");
-    exit(1);
-
-}
-printf("\nEnter name of person to search\n");
-got(name);
-while(fread(&p,sizeof(p),1,f)==1)
-{
-    if(strcmp(p.name,name)==0)
+  if(n2<0) printf("Second number should be +ve.");
+  else
+  {
+    float result=1.0;
+    for(int i=1; i<=n2; i++)
     {
-        printf("\n\tDetail Information About %s",name);
-        printf("\nName:%s\naddress:%s\nFather name:%s\nMother name:%s\nMobile no:%ld\nsex:%s\nE-mail:%s\nCitision no:%s",p.name,p.address,p.father_name,p.mother_name,p.mble_no,p.sex,p.mail,p.citision_no);
+       result *= n1;
     }
-        else
-        printf("file not found");
-
-}
- fclose(f);
-  printf("\n Enter any key");
-
-	 getch();
-    system("cls");
-menu();
-}
-void deleterecord()
-{
-    struct person p;
-    FILE *f,*ft;
-	int flag;
-	char name[100];
-	f=fopen("project","rb");
-	if(f==NULL)
-		{
-
-			printf("CONTACT'S DATA NOT ADDED YET.");
-
-		}
-	else
-	{
-		ft=fopen("temp","wb+");
-		if(ft==NULL)
-
-            printf("file opaning error");
-		else
-
-        {
-
-
-		printf("Enter CONTACT'S NAME:");
-		got(name);
-
-		fflush(stdin);
-		while(fread(&p,sizeof(p),1,f)==1)
-		{
-			if(strcmp(p.name,name)!=0)
-				fwrite(&p,sizeof(p),1,ft);
-			if(strcmp(p.name,name)==0)
-                flag=1;
-		}
-	fclose(f);
-	fclose(ft);
-	if(flag!=1)
-	{
-		printf("NO CONACT'S RECORD TO DELETE.");
-		remove("temp.txt");
-	}
-		else
-		{
-			remove("project");
-			rename("temp.txt","project");
-			printf("RECORD DELETED SUCCESSFULLY.");
-
-		}
-	}
-}
- printf("\n Enter any key");
-
-	 getch();
-    system("cls");
-menu();
-}
-
-void modifyrecord()
-{
-    int c;
-    FILE *f;
-    int flag=0;
-    struct person p,s;
-	char  name[50];
-	f=fopen("project","rb+");
-	if(f==NULL)
-		{
-
-			printf("CONTACT'S DATA NOT ADDED YET.");
-			exit(1);
-
-
-		}
-	else
-	{
-	    system("cls");
-		printf("\nEnter CONTACT'S NAME TO MODIFY:\n");
-            got(name);
-            while(fread(&p,sizeof(p),1,f)==1)
-            {
-                if(strcmp(name,p.name)==0)
-                {
-
-
-
-                    printf("\n Enter name:");
-                    got(s.name);
-                    printf("\nEnter the address:");
-                    got(s.address);
-                    printf("\nEnter father name:");
-                    got(s.father_name);
-                    printf("\nEnter mother name:");
-                    got(s.mother_name);
-                    printf("\nEnter phone no:");
-                    scanf("%ld",&s.mble_no);
-                    printf("\nEnter sex:");
-                    got(s.sex);
-                    printf("\nEnter e-mail:");
-                    got(s.mail);
-                    printf("\nEnter citizen no\n");
-                    got(s.citision_no);
-                    fseek(f,-sizeof(p),SEEK_CUR);
-                    fwrite(&s,sizeof(p),1,f);
-                    flag=1;
-                    break;
-
-
-
-                }
-                fflush(stdin);
-
-
-            }
-            if(flag==1)
-            {
-                printf("\n your data id modified");
-
-            }
-            else
-            {
-                    printf(" \n data is not found");
-
-            }
-            fclose(f);
-	}
-	 printf("\n Enter any key");
-	 getch();
-    system("cls");
-	menu();
-
-}
-void got(char *name)
-{
-
-   int i=0,j;
-    char c,ch;
-    do
-    {
-        c=getch();
-                if(c!=8&&c!=13)
-                {
-                    *(name+i)=c;
-                        putch(c);
-                        i++;
-                }
-                if(c==8)
-                {
-                    if(i>0)
-                    {
-                        i--;
-                    }
-                   // printf("h");
-                    system("cls");
-                    for(j=0;j<i;j++)
-                    {
-                        ch=*(name+j);
-                        putch(ch);
-
-                    }
-
-                }
-    }while(c!=13);
-      *(name+i)='\0';
+    display(n1, n2, '^', result);
+  }
 }
